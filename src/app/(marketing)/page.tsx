ee0b1b1cec1b4e40693edef2607d093f15571e44
dillonzellis <dillonzellis@gmail.com>
2024-03-24 19:58:46 -0400

    style: marketing page minor styling

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -14,7 +14,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center py-4 gap-2">
       <Image
         src={hero}
         alt="Hero"
@@ -23,11 +23,7 @@ export default function Home() {
       <div className="flex flex-col items-center gap-y-8">
         <h1 className="text-xl font-bold text-neutral-600 max-w-[480px] text-center">
           Level up and improve your language skills with{" "}
-          <span>
-            {/* <Image src="/sword.svg" height={24} width={24} alt="" /> */}
-
-            <span className="italic font-extrabold">SpeechCraft.</span>
-          </span>
+          <span className="italic font-extrabold">SpeechCraft.</span>
         </h1>
         <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
           <ClerkLoading>
--- src/components/layouts/MarketingHeader.tsx
+++ src/components/layouts/MarketingHeader.tsx
@@ -14,7 +14,6 @@ export const MarketingHeader = () => {
     <header className="w-full h-20 border-b-2 border-slate-200 px-4">
       <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
         <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
-          <div>Icon</div>
           <h1 className="text-2xl font-bold">SpeechCraft</h1>
         </div>
         <ClerkLoading>
