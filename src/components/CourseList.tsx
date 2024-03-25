89b207c82d0a6c6e634477b7929a41e27b79787f
dillonzellis <dillonzellis@gmail.com>
2024-03-25 14:50:33 -0400

    feat: userprogress active course action

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { CourseCard } from "./CourseCard";
-
 import { courses, userProgress } from "@/db/schema";
+import { useTransition } from "react";
+import { useRouter } from "next/navigation";
+import { upsertUserProgress } from "@/actions/user-progress";
 
 interface ListProps {
   courses: (typeof courses.$inferSelect)[];
@@ -10,6 +12,21 @@ interface ListProps {
 }
 
 export const CoursesList = ({ courses, activeCourseId }: ListProps) => {
+  const router = useRouter();
+  const [pending, startTransition] = useTransition();
+
+  const onClick = (id: number) => {
+    if (pending) return;
+
+    if (id === activeCourseId) {
+      return router.push("/learn");
+    }
+
+    startTransition(() => {
+      upsertUserProgress(id);
+    });
+  };
+
   return (
     <div className="pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4">
       {courses.map((course) => (
@@ -18,8 +35,8 @@ export const CoursesList = ({ courses, activeCourseId }: ListProps) => {
           id={course.id}
           title={course.title}
           imageSrc={course.imageSrc}
-          onClick={() => {}}
-          disabled={false}
+          onClick={onClick}
+          disabled={pending}
           active={course.id === activeCourseId}
         />
       ))}
