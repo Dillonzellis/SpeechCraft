5d2ead167e1c9398a78e964c76afcb91844a4d60
dillonzellis <dillonzellis@gmail.com>
2024-03-25 12:13:53 -0400

    feat: add layout components for learn page

--- src/app/(dashboard)/learn/page.tsx
+++ src/app/(dashboard)/learn/page.tsx
@@ -1,3 +1,17 @@
+import { FeedWrapper } from "@/components/FeedWrapper";
+import { StickyWrapper } from "@/components/StickyWrapper";
+import { UserProgress } from "@/components/UserProgress";
+import { LearnHeader } from "@/components/layouts/LearnHeader";
+
 export default function LearnPage() {
-  return <div className="flex flex-row-reverse gap-[48px] px-6"></div>;
+  return (
+    <div className="flex gap-[48px] px-6">
+      <FeedWrapper>
+        <LearnHeader title="Learn Spanish" />
+      </FeedWrapper>
+      <StickyWrapper>
+        <UserProgress points={100} hearts={5} />
+      </StickyWrapper>
+    </div>
+  );
 }
