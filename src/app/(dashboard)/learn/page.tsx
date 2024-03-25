f1c69e1dc557b9300a7fcaf97af038c6a9cf9675
dillonzellis <dillonzellis@gmail.com>
2024-03-25 14:24:46 -0400

    feat: add redirect if no user progress

--- src/app/(dashboard)/learn/page.tsx
+++ src/app/(dashboard)/learn/page.tsx
@@ -2,8 +2,19 @@ import { FeedWrapper } from "@/components/FeedWrapper";
 import { StickyWrapper } from "@/components/StickyWrapper";
 import { UserProgress } from "@/components/UserProgress";
 import { LearnHeader } from "@/components/layouts/LearnHeader";
+import { redirect } from "next/navigation";
 
-export default function LearnPage() {
+import { getUserProgress } from "@/db/queries";
+
+export default async function LearnPage() {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
   return (
     <div className="flex gap-[48px] px-6">
       <FeedWrapper>
